945223af1f9aa743ff11bc7905b263abe255a9ee
prakashponali <p.ponali@gamil.com>
2023-12-03 18:14:16 +0530

    Contest problem

---
+++ advance/src/test/java/org/example/BeggersAndDivotessArrayProblemTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Author prakashponali
+ * @Date 03/12/23
+ * @Description
+ */
+class BeggersAndDivotessArrayProblemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
