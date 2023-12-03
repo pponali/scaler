dba0e75261f3bb27007aec79b753e3c2c149816e
prakashponali <p.ponali@gamil.com>
2023-12-03 18:17:46 +0530

    Refactoring

--- advance/src/main/java/com/practice/day12/additional/SumOfNonNegative.java
+++ advance/src/main/java/com/practice/day12/additional/SumOfNonNegative.java
@@ -1,16 +1,14 @@
 package com.practice.day12.additional;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Hashtable;
 
 /**
  * @Author prakashponali
  * @Date 14/11/23
  * @Description
  */
-public class SUmOfNonNegetive {
+public class SumOfNonNegative {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(solve(new int[]{1, 0, -1}, new int[][]{{0, 2}})));
