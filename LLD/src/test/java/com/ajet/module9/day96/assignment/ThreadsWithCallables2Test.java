de5ad5e28956a29e5b841f3d5fe8e8f1ec0b6496
prakashponali <p.ponali@gamil.com>
2024-07-15 16:07:07 +0530

    day96 practice test cases.

---
+++ LLD/src/test/java/com/ajet/module9/day96/assignment/ThreadsWithCallables2Test.java
@@ -0,0 +1,44 @@
+package com.ajet.module9.day96.assignment;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Author prakashponali @pponali
+ * <p>
+ * Date   15/07/24 3:52 pm
+ */
+public class ThreadsWithCallables2Test {
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    public void testCallables() {
+        ThreadsWithCallables2.main(new String[]{});
+    }
+
+    @Test
+    public void testCallables2() {
+        ThreadsWithCallables2.main(new String[]{});
+    }
+
+    @Test
+    public void testCallables3() {
+        ThreadsWithCallables2.main(new String[]{});
+    }
+
+    @Test
+    public void testCallables4() {
+        ThreadsWithCallables2.main(new String[]{});
+    }
+}
