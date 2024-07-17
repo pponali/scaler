95015ccde7c841a1156fc80a767b485479ab44e4
prakashponali <p.ponali@gamil.com>
2024-07-17 14:57:20 +0530

    db module creation

---
+++ dbsql/src/main/java/com/ajet/Main.java
@@ -0,0 +1,12 @@
+package com.ajet;
+
+/**
+ * Author prakashponali @pponali
+ * <p>
+ * Date   17/07/24 2:52 pm
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
