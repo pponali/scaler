6296b5c5bd8f79f3e8073d790ad073ea51ae76e3
p.ponali@gmail.com <P@kash231>
2024-07-24 06:57:20 +0530

    Connection pool implementation

--- LLD/src/main/java/com/ajet/module10/day107/assignment/designpatterns/singleton/SingletonPool/ConnectionPoolImpl.java
+++ LLD/src/main/java/com/ajet/module10/day107/assignment/designpatterns/singleton/SingletonPool/ConnectionPoolImpl.java
@@ -1,29 +1,67 @@
 package com.ajet.module10.day107.assignment.designpatterns.singleton.SingletonPool;
 
-public class ConnectionPoolImpl implements ConnectionPool{
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionPoolImpl implements ConnectionPool {
+    private static ConnectionPoolImpl instance;
+    private final List<DatabaseConnection> connections;
+    private final int maxConnections;
+
+    private ConnectionPoolImpl(int maxConnections) {
+        this.maxConnections = maxConnections;
+        this.connections = new ArrayList<>();
+        initializePool();
+    }
+
+    public static synchronized ConnectionPoolImpl getInstance(int maxConnections) {
+        if (instance == null) {
+            instance = new ConnectionPoolImpl(maxConnections);
+        }
+        return instance;
+    }
+
+    public static synchronized void resetInstance() {
+        instance = null;
+    }
 
     @Override
     public void initializePool() {
-
+        for (int i = 0; i < maxConnections; i++) {
+            connections.add(new DatabaseConnection());
+        }
     }
 
     @Override
-    public DatabaseConnection getConnection() {
-        return null;
+    public synchronized DatabaseConnection getConnection() {
+        for (DatabaseConnection connection : connections) {
+            if (connection.isAvailable()) {
+                connection.setAvailable(false);
+                return connection;
+            }
+        }
+        return null; // or throw an exception if preferred
     }
 
     @Override
-    public void releaseConnection(DatabaseConnection connection) {
-
+    public synchronized void releaseConnection(DatabaseConnection connection) {
+        connection.setAvailable(true);
     }
 
     @Override
-    public int getAvailableConnectionsCount() {
-        return 0;
+    public synchronized int getAvailableConnectionsCount() {
+        int count = 0;
+        for (DatabaseConnection connection : connections) {
+            if (connection.isAvailable()) {
+                count += 1;
+            }
+        }
+        return count;
     }
 
     @Override
-    public int getTotalConnectionsCount() {
-        return 0;
+    public synchronized int getTotalConnectionsCount() {
+        return connections.size();
     }
 }
+
--- LLD/src/main/java/com/ajet/module10/day107/assignment/designpatterns/singleton/SingletonPool/DatabaseConnection.java
+++ LLD/src/main/java/com/ajet/module10/day107/assignment/designpatterns/singleton/SingletonPool/DatabaseConnection.java
@@ -1,4 +1,18 @@
 package com.ajet.module10.day107.assignment.designpatterns.singleton.SingletonPool;
 
 public class DatabaseConnection {
+    private boolean isAvailable;
+
+    public DatabaseConnection() {
+        this.isAvailable = true;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
 }
+
