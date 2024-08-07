cf8b9cfd143c2f734c804e2a6ef84a43790fa40e
Ponali Prakash <ponali.prakash@accenture.com>
2024-08-07 14:29:34 +0530

    day 111 adapter.

--- LLD/src/main/java/com/ajet/module10/day111/designpatterns/adapter/session/PhonePe.java
+++ LLD/src/main/java/com/ajet/module10/day111/designpatterns/adapter/session/PhonePe.java
@@ -1,11 +1,9 @@
 package com.ajet.module10.day111.designpatterns.adapter.session;
 
-import com.ajet.module10.day111.designpatterns.adapter.session.yesbank.YesBank;
-
 public class PhonePe {
 
     public static void main(String[] args) {
-        UPIBankInterface bank = new YesBank();
+        UPIBankInterface bank = null; //= new YesBank();
         bank.pay("1234567890", 1000);
         bank.receive("1234567890", 1000);
     }
