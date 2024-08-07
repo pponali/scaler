db754860649fe811e8d6bde65b6d9fc851ba45ee
Ponali Prakash <ponali.prakash@accenture.com>
2024-08-07 14:49:03 +0530

    day 111 adapter.

--- LLD/src/main/java/com/ajet/module10/day113/designpatterns/behavioural/session/observer/Client.java
+++ LLD/src/main/java/com/ajet/module10/day113/designpatterns/behavioural/session/observer/Client.java
@@ -4,7 +4,7 @@ public class Client {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
         new ForecastDisplay(weatherData);
-        new StasticsDisplay(weatherData);
+        new StatisticsDisplay(weatherData);
         weatherData.setMeasurements(30, 65, 30.4f);
         weatherData.setMeasurements(32, 70, 29.2f);
         weatherData.setMeasurements(28, 90, 29.2f);
